fc3d6dc608cdd600e8d3206e82fa011bca92d8d5
Artemiy <Artemiy.Dmitrochenko@gmail.com>
2019-10-21 19:19:38 +0500

    JS on work (don't work storage)

--- js/script.js
+++ js/script.js
@@ -8,10 +8,25 @@ var userName = popupWriteUs.querySelector("[name=name]");
 var email = popupWriteUs.querySelector("[name=email]");
 var message = popupWriteUs.querySelector("[name=message]");
 
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("userName");
+} catch (err) {
+  isStorageSupport = false;
+}
+
 writeUsLink.addEventListener("click", function (evt) {
   evt.preventDefault();
   popupWriteUs.classList.add("modal-show");
-  userName.focus();
+
+  if (storage) {
+      userName.value = storage.getItem("userName");
+      email.value = storage.getItem("email");
+  } else {
+    userName.focus();
+  }
 });
 
 close.addEventListener("click", function (evt) {
@@ -20,13 +35,17 @@ close.addEventListener("click", function (evt) {
 });
 
 formWriteUs.addEventListener("submit", function (evt) {
-  if (!userName.value || !validate(email.value) || !message.value) {
+  if (!userName.value || validate(email.value) || !message.value) {
     evt.preventDefault();
-    console.log("Нужно заполнить все поля");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("userName", userName.value);
+      localStorage.setItem("email", email.value);
+    }
   }
 });
 
 function validate(email) {
    var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-   return reg.test(email) == false
+   return reg.test(email) == false;
 }
